a9a1f2174b11029a6eaf6ea870e72c7f25e69c05
DomsQi <neonburnt@yahoo.com>
2018-10-27 16:40:34 +0700

    Add project files.

---
+++ Exer01_HelloWorld/Exer01_HelloWorld.cpp
@@ -0,0 +1,28 @@
+/*	
+	Project Name			: Exer01_HelloWorld.cpp	
+	Project Description		: Defines the entry point for the console application.
+	Author Name				: Dominador V. Nazareno Jr.
+	Date Written/Time		: 10 Oct 2018 09:48 AM
+	Build Date				: 10 Oct 2018 09:48 AM
+	Initial Code Version	: 1.0
+	Remarks					: Exercise 01
+	-------------------------------------------------------------------------------
+	Modification Logs	:
+	-------------------------------------------------------------------------------
+	DD MMM YYYY		    : Change Logs
+	-------------------------------------------------------------------------------
+	10 OCT 2018			: Initial Release
+	
+	-------------------------------------------------------------------------------
+*/
+
+#include "stdafx.h"
+#include "stdio.h"
+
+int main()
+{
+	printf("Hello World!\nThis is my simple demo.\n");
+	_gettche();
+    return 0;
+}
+
